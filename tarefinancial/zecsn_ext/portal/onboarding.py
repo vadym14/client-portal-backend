5143b939db91bd8ec9a61c13030539b61bef9fbd
vadym14 <vadym.work14@gmail.com>
2022-07-14 23:34:11 +0500

    :building_construction: Improve account verification

--- tarefinancial/zecsn_ext/portal/onboarding.py
+++ tarefinancial/zecsn_ext/portal/onboarding.py
@@ -4,10 +4,10 @@ from frappe import _
 
 @frappe.whitelist(allow_guest=True)
 def account_info(name, date_of_birth, ssn):
-    customer = frappe.get_doc('Customer', {"name": name, "date_of_birth": date_of_birth})
+    customer = frappe.get_doc('Customer', {"name": name})
     if not customer:
-        return {'status': False, 'message': _("Customer does not exist")}
-    elif customer.get_password('ssn')[-4:] == ssn:
+        return {'status': False, 'message': _("Account does not exist")}
+    elif customer.get_password('ssn')[-4:] == ssn and customer.date_of_birth.strftime('%Y-%m-%d') == date_of_birth:
         return {'status': True, 'message': name}
     else:
-        return {'status': False, 'message': _("Incorrect SSN")}
+        return {'status': False, 'message': _("Incorrect account information")}
